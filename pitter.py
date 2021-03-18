6555d0295fd1b712251ad686d2e60f8aaf93e346
Riquelm <41448408+Scratchewan@users.noreply.github.com>
2021-03-18 14:38:23 +0100

    Session

--- pitter.py
+++ pitter.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_cors import CORS
 import os
+from datetime import timedelta
 
 application = Flask(__name__)
+application.secret_key = "cute"
+application.permanent_session_lifetime = timedelta(days=5)
 
 cors = CORS(application, resourse={r"/*":{"origins":"*"}})
 
@@ -24,14 +27,32 @@ def about():
 @application.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
+        session.permanent = True
         user = request.form["nm"]
-        return redirect(url_for("user", usr=user))
+        session["user"] = user
+        # return redirect(url_for("user", usr=user))
+        return redirect(url_for("user"))
     else:
+        if "user" in session:
+            return redirect(url_for("user"))
         return render_template("login.html")
 
-@application.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
+# @application.route("/<usr>")
+# def user(usr):
+#     return f"<h1>{usr}</h1>"
+
+@application.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return f"<h1>{user}</h1>"
+    else:
+        return redirect(url_for("login"))
+
+@application.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
 
 def main():
     port = int(os.environ.get("PORT", 5000))
