e2f0afd55bd5f34fdabe9b99f6987db3644096ba
Riquelm <41448408+Scratchewan@users.noreply.github.com>
2021-03-20 16:02:10 +0100

    Base

--- HoHey/__init__.py
+++ HoHey/__init__.py
@@ -1,14 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+import os
 from flask_login import LoginManager
+# import sqlite3
+
+# current_directory = os.path.dirname(os.path.abspath(__file__))
 
 database = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     application = Flask(__name__)
     application.config['SECRET_KEY'] = 'cute'
     application.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost:5432/Ho Hey'
+    # application.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     database.init_app(application)
 
@@ -33,5 +39,5 @@ def create_app():
     return application
 
 def create_database(application):
-    if not path.exists('website/database.db'):
+    if not path.exists('HoHey/' + DB_NAME):
         database.create_all(app=application)
