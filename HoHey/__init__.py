95dea748fcb0b42bafe2e754283b7407f8ee5ef2
Riquelm <41448408+Scratchewan@users.noreply.github.com>
2021-03-23 17:47:19 +0100

    Update Pipfile

--- HoHey/__init__.py
+++ HoHey/__init__.py
@@ -4,24 +4,19 @@ from os import path
 import os
 from flask_login import LoginManager
 
-ENV = 'dev'
-
 database = SQLAlchemy()
 DB_NAME = "database.db"
 
 def create_app():
     application = Flask(__name__)
 
-    if ENV == 'dev':
-        application.debug = True
-        # application.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost:5432/Ho Hey'
-        application.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    else:
-        application.debug = False
-        application.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ktjdyajigzjswv:ee6a1dfae558349472a7d3501cc77ea21b8a58fbb4d15ba19f8d6d7a1241e770@ec2-34-195-233-155.compute-1.amazonaws.com:5432/d371j372uho6t8'
-
     application.config['SECRET_KEY'] = 'cute'
-    application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # application.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost:5432/Ho Hey'
+    application.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    # application.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ktjdyajigzjswv:ee6a1dfae558349472a7d3501cc77ea21b8a58fbb4d15ba19f8d6d7a1241e770@ec2-34-195-233-155.compute-1.amazonaws.com:5432/d371j372uho6t8'
+    
+    # application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     database.init_app(application)
 
     from .views import views
--- Procfile
+++
@@ -1,1 +0,0 @@
-app: python pitter.py
