49c372313e3a6adf742b4fec4f1507ccdc8dfca3
Riquelm <41448408+Scratchewan@users.noreply.github.com>
2021-03-26 01:06:35 +0100

    Update .gitignore

--- HoHey/auth.py
+++ HoHey/auth.py
@@ -3,6 +3,7 @@ from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import database
 from flask_login import login_user, login_required, logout_user, current_user
+# from flask_mail import Mail
 
 auth = Blueprint('auth', __name__)
 
