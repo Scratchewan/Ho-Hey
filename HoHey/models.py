16cfade1bdd60d1187749a664638d56d11abef52
Riquelm <41448408+Scratchewan@users.noreply.github.com>
2021-03-26 16:59:03 +0100

    Update models.py

--- HoHey/models.py
+++ HoHey/models.py
@@ -3,12 +3,14 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 class Note(database.Model):
+    __tablename__ = 'note'
     id = database.Column(database.Integer, primary_key=True)
     data = database.Column(database.String(16384))
     date = database.Column(database.DateTime(timezone=True), default=func.now())
     user_id = database.Column(database.Integer, database.ForeignKey('user.id'))
 
 class User(database.Model, UserMixin):
+    __tablename__ = 'user'
     id = database.Column(database.Integer, primary_key=True)
     email = database.Column(database.String(128), unique=True)
     password = database.Column(database.String(128))
